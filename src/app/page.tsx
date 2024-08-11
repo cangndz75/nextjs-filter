14a905ee884d46c0cff10cab96f73fe8cc458c0c
Can Gündüz <cangunduz0001@gmail.com>
2024-08-11 16:00:32 +0300

    react-query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 "use client"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Product } from "@/db";
 import { cn } from "@/lib/utils";
+import { useQuery } from "@tanstack/react-query";
+import { QueryResult } from "@upstash/vector";
+import axios from "axios";
 import { ChevronDown, Filter } from "lucide-react";
 import { useState } from "react";
 
@@ -22,7 +26,23 @@ export default function Home() {
   const [filter,setFilter] = useState({
     sort:'none',
   });
-  console.log(filter);
+
+  const {data:products} = useQuery({
+    queryKey:["products"],
+    queryFn:async() => {
+      const {data} = await axios.post<QueryResult<Product>[]>(
+        'http://localhost:3000/api/products',{
+          filter:{
+            sort:filter.sort,
+          }
+        }
+      )
+      return data
+    }
+  })
+
+  console.log(products);
+
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
